375fd3cbbc34e90baab85845c4dcadf8d443c399
nicolas <nicolas@homework.(none)>
2012-12-10 23:39:26 +0100

    ajout des filtres twig utiles, traductions et creation bundle news

---
+++ src/NP/Bundle/GalleryBundle/Form/Handler/GalleryGroupFormHandler.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace NP\Bundle\GalleryBundle\Form\Handler;
+
+class GalleryGroupFormHandler extends \NP\Bundle\CoreBundle\Form\Handler\BaseGroupFormHandler {
+    protected $repository_name = 'NPGalleryBundle:Gallery';
+}
---
+++ src/NP/Bundle/NewsBundle/NPNewsBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace NP\Bundle\NewsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class NPNewsBundle extends Bundle
+{
+}
